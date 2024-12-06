a03259ec164b11c6c6255580310f0800df83c443
Gourav Makode <140704003+GT0SRT@users.noreply.github.com>
2024-12-06 11:43:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import forms from '@tailwindcss/forms';  // Use `import` instead of `require`
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [
-    require('@tailwindcss/forms'),
+    forms,  // Use the imported variable directly here
   ],
   theme: {
     extend: {},
