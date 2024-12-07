bc6e027a89322ffb2f46e53dff09d1fce0e160f8
bluea1855 <fakemail1855@gmail.com>
2024-12-07 22:29:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { Disclosure, Menu } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { FaToggleOn } from "react-icons/fa";
 import { FaToggleOff } from "react-icons/fa";
+// import { useState } from 'react';
 import { toggleClick } from './redux/clickSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -15,19 +16,19 @@ const user = {
   email: 'tom@example.com',
   imageUrl:
     'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-};
+}
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
   { name: 'About Developer', href: '#', current: false },
-];
+]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
-];
+]
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ');
+  return classes.filter(Boolean).join(' ')
 }
 
 const App = () => {
@@ -40,13 +41,15 @@ const App = () => {
 
   return (
     <>
-      <div className="min-h-full bg-gradient-to-l from-white to-slate-300">
-        <Disclosure as="nav" className={isClicked ? 'bg-gray-800' : 'bg-white'}>
+      <div className={isClicked? 'min-h-full bg-gradient-to-l from-white to-slate-300':
+        'min-h-full bg-gradient-to-l from-gray-800 to-gray-600'}>
+        
+        <Disclosure as="nav" className={isClicked? 'bg-white':'bg-gray-800'}>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex h-16 items-center justify-between">
               <div className="flex items-center">
                 <div className="flex-shrink-0">
-                  <p className={isClicked ? 'text-white flex flex-col justify-center items-center text-xl h-8 w-fit font-bold' : 'text-black flex flex-col justify-center items-center text-xl h-8 w-fit font-bold'}>React-Chat</p>
+                <p className={isClicked? 'text-white flex flex-col justify-center items-center text-xl h-8 w-fit font-bold':'text-black flex flex-col justify-center items-center text-xl h-8 w-fit font-bold'}>React-Chat</p>
                 </div>
                 <div className="hidden md:block">
                   <div className="ml-10 flex items-baseline space-x-4">
@@ -67,7 +70,7 @@ const App = () => {
                 </div>
               </div>
               <div className='ml-auto cursor-pointer'>
-                {isClicked ? <FaToggleOn className='text-white' size={32} onClick={handleToggle} /> : <FaToggleOff size={32} onClick={handleToggle} />}
+                {isClicked? <FaToggleOn className='text-white' size={32} onClick={handleToggle}/>:<FaToggleOff size={32} onClick={handleToggle}/>}
               </div>
               <div className="hidden md:block">
                 <div className="ml-4 flex items-center md:ml-6">
@@ -83,42 +86,41 @@ const App = () => {
                   {/* Profile dropdown */}
                   <Menu as="div" className="relative ml-3">
                     <div>
-                      <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                         <span className="absolute -inset-1.5" />
                         <span className="sr-only">Open user menu</span>
                         <img alt="" src={user.imageUrl} className="h-8 w-8 rounded-full" />
-                      </Menu.Button>
+                      </MenuButton>
                     </div>
-                    <Menu.Items
-                      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-                    >
-                      {userNavigation.map((item) => (
-                        <Menu.Item key={item.name}>
-                          <a href={item.href} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
-                            {item.name}
-                          </a>
-                        </Menu.Item>
-                      ))}
-                    </Menu.Items>
+                    <MenuItems transition
+                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
+                    {userNavigation.map((item) => (
+                      <MenuItem key={item.name}>
+                        <a href={item.href} className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                          {item.name}
+                        </a>
+                      </MenuItem>
+                    ))}
+                    </MenuItems>
                   </Menu>
                 </div>
               </div>
               <div className="-mr-2 flex md:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
                   <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
                   <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
-                </Disclosure.Button>
+                </DisclosureButton>
               </div>
             </div>
           </div>
 
-          <Disclosure.Panel className="md:hidden">
+          <DisclosurePanel className="md:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <DisclosureButton
                   key={item.name}
                   as="a"
                   href={item.href}
@@ -129,7 +131,7 @@ const App = () => {
                   )}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </DisclosureButton>
               ))}
             </div>
             <div className="border-t border-gray-700 pb-3 pt-4">
@@ -152,29 +154,32 @@ const App = () => {
               </div>
               <div className="mt-3 space-y-1 px-2">
                 {userNavigation.map((item) => (
-                  <Disclosure.Button
+                  <DisclosureButton
                     key={item.name}
                     as="a"
                     href={item.href}
                     className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                   >
                     {item.name}
-                  </Disclosure.Button>
+                  </DisclosureButton>
                 ))}
               </div>
             </div>
-          </Disclosure.Panel>
+          </DisclosurePanel>
         </Disclosure>
         <Banner />
-        <div className="mx-auto w-full pl-12 py-5 pb-2 bg-white">
-          <h1 className="text-3xl font-semibold tracking-tight text-slate-700">Chat Dashboard</h1>
-        </div>
+        {/* <header className="bg-gradient-to-l from-blue-200 via-pink-300 to-blue-700 shadow"> */}
+          <div className="mx-auto w-full pl-12 py-5 pb-2 bg-white">
+            <h1 className="text-3xl font-semibold tracking-tight text-slate-700">Chat Dashboard</h1>
+          </div>
+        {/* </header> */}
+        
         <main>
             <Dashboard />  
         </main>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
