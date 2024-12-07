f2d292116b173ea718d61d7a4711c809429c64f0
bluea1855 <fakemail1855@gmail.com>
2024-12-07 21:31:53 +0530

    Add files via upload

---
+++ src/redux/clickSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  isClicked: false,
+};
+
+const clickSlice = createSlice({
+  name: 'click', 
+  initialState,
+  reducers: {
+    toggleClick: (state) => {
+      state.isClicked = !state.isClicked; 
+    },
+  },
+});
+
+export const { toggleClick } = clickSlice.actions;
+export default clickSlice.reducer;
