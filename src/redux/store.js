478086c243c4e6385391645fc16cd77a83e2c425
bluea1855 <fakemail1855@gmail.com>
2024-12-07 21:31:05 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
-import connectsReducer from './connectsSlice'
-import commentsReducer from './commentsSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import connectsReducer from './connectsSlice';
+import commentsReducer from './commentsSlice';
+import clickReducer from './clickSlice'; 
 
 export default configureStore({
   reducer: {
     connects_redux_slice: connectsReducer,
     comments_redux_slice: commentsReducer,
+    click_redux_slice: clickReducer,
   },
-})
+});
