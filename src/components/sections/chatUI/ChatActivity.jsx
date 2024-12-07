92c75eb006e8299484bc9fde3015b0d1f0e30f8e
bluea1855 <fakemail1855@gmail.com>
2024-12-07 22:00:58 +0530

    Update ChatActivity.jsx

--- src/components/sections/chatUI/ChatActivity.jsx
+++ src/components/sections/chatUI/ChatActivity.jsx
@@ -1,14 +1,16 @@
-export default function ChatActivity(props) {
+// import { isCookie } from "react-router-dom"
+import { useSelector } from 'react-redux';
 
-    const people = props.connects
-  
-  
+export default function ChatActivity(props) {
+    const isClicked = useSelector((state) => state.click_redux_slice.isClicked);
+    const people = props.connects ?? [];
+
     return (
       <ul
         role="list"
-        className="divide-y bg-white p-4 rounded-3xl divide-gray-100"
+        className={isClicked ? 'divide-y bg-white p-4 rounded-3xl divide-gray-100' : 'divide-y bg-white p-4 rounded-3xl divide-gray-100 bg-black text-white'}
       >
-        {people.map((person, index) => (
+        {people?.map((person, index) => (
           <li key={index} className="flex justify-between gap-x-6 py-5">
             <div className="flex min-w-0 gap-x-4">
               <img
@@ -17,18 +19,18 @@ export default function ChatActivity(props) {
                 className="h-12 w-12 flex-none rounded-full bg-gray-50"
               />
               <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">
+                <p className={isClicked ? 'text-sm font-semibold leading-6 text-gray-900':'text-sm font-semibold leading-6 text-gray-300'}>
                   {person.name}
                 </p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                <p className={isClicked ? 'mt-1 truncate text-xs leading-5 text-gray-500':'mt-1 truncate text-xs leading-5 text-gray-700'}>
                   {person.email}
                 </p>
               </div>
             </div>
             <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+              <p className={isClicked ? 'text-sm leading-6 text-gray-900':'text-sm leading-6 text-gray-300'}>{person.role}</p>
               {person.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
+                <p className={isClicked ? 'mt-1 text-xs leading-5 text-gray-500':'mt-1 text-xs leading-5 text-gray-700'}>
                   Last seen{" "}
                   <time dateTime={person.lastSeenDateTime}>
                     {person.lastSeen}
