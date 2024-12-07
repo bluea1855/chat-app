bd5709101d9a6fece21eca2cf06a1c7cf9cc06f3
bluea1855 <fakemail1855@gmail.com>
2024-12-08 00:16:48 +0530

    Update BlogsSection.jsx

--- src/components/BlogsSection.jsx
+++ src/components/BlogsSection.jsx
@@ -89,7 +89,8 @@ const posts = [
                   </a>
                 </div>
                 <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <h3 className={isClicked ? 'mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600':
+                    'mt-3 text-lg font-semibold leading-6 text-slate-100 group-hover:text-gray-600'}>
                     <a href={post.href}>
                       <span className="absolute inset-0" />
                       {post.title}
