afdce7d4de25adfef7f66144e5054820febcfb5f
bluea1855 <fakemail1855@gmail.com>
2024-12-07 23:24:42 +0530

    Update Dashboard.jsx

--- src/components/sections/dashboard/Dashboard.jsx
+++ src/components/sections/dashboard/Dashboard.jsx
@@ -10,7 +10,7 @@ import { useSelector } from 'react-redux';
 
 function Dashboard() {
   const isClicked = useSelector((state) => state.click_redux_slice.isClicked);
-  
+
   return (
     <>
       <PageWrapper
@@ -35,21 +35,21 @@ function Dashboard() {
         <div className="w-96 shadow-lg flex flex-col justify-center items-center gap-10 text-white bg-slate-400 p-6 rounded-2xl">
           <CreateAnAccount hideButton />
 
-          <div className="bg-slate-200 rounded-3xl shadow-md p-6 text-black text-xl">
+          <div className={isClicked ? 'bg-slate-200 rounded-3xl shadow-md p-6 text-black text-xl':'bg-black rounded-3xl shadow-md p-6 text-slate-200 text-xl'}>
           Quickly set up your profile and join our growing community. Enjoy easy access to all features and start connecting with others in no time. Your privacy and security are our top priority.
           </div>
         </div>
         <div className="w-96 shadow-lg flex flex-col justify-center items-center gap-10 text-white bg-slate-400 p-6 rounded-2xl">
           <ContinueSignin hideButton />
 
-          <div className="bg-slate-200 rounded-3xl shadow-md p-6 text-black text-xl">
+          <div className={isClicked ? 'bg-slate-200 rounded-3xl shadow-md p-6 text-black text-xl':'bg-black rounded-3xl shadow-md p-6 text-slate-200 text-xl'}>
           Pick up right where you left off by signing in securely. Stay connected and have seamless access to your messages, preferences, and conversations, without any hassle.
           </div>
         </div>
         <div className="w-96 shadow-lg flex flex-col justify-center items-center gap-10 text-white bg-slate-400 p-6 rounded-2xl">
           <ChatAnonymous hideButton />
-
-          <div className="bg-slate-200 rounded-3xl shadow-md p-6 text-black text-xl">
+          
+          <div className={isClicked ? 'bg-slate-200 rounded-3xl shadow-md p-6 text-black text-xl':'bg-black rounded-3xl shadow-md p-6 text-slate-200 text-xl'}>
           Talk freely and securely with others without revealing your identity. Our platform ensures full anonymity, allowing you to express yourself comfortably while staying protected.
           </div>
         </div>
