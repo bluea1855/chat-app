4e2695ca4b0a27adaaa05e13b59d461d093b43be
bluea1855 <fakemail1855@gmail.com>
2024-12-08 14:45:47 +0530

    Update signIn.jsx

--- src/components/sections/signIn/signIn.jsx
+++ src/components/sections/signIn/signIn.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router";
-
 import {useRef} from 'react'
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 const validUser = "react_chat@rc.com";
 const validPassword = "react123";
@@ -20,7 +20,6 @@ export default function SignIn() {
       return;
     }
 
-
     alert('Not a valid registered user!');
   }
 
@@ -33,12 +32,18 @@ export default function SignIn() {
     password.current= event.target.value;
   }
 
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+  const isClicked = useSelector((state) => state.click_redux_slice.isClicked);
+
   return (
     <>
       <div className="flex bg-gradient-to-r from-blue-900 via-pink-400 to-indigo-700 h-screen w-screen flex-col justify-center items-center">
-        <div className="bg-white flex flex-col justify-center items-center rounded-xl shadow-3xl w-fit p-7">
+      <div className={classNames( isClicked ? 'bg-white':'bg-gray-900','flex flex-col justify-center items-center rounded-xl shadow-3xl w-fit p-7',)}>
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className={classNames( isClicked ? 'text-grsy-900':'text-white', 'mt-10 text-center text-2xl font-bold leading-9 tracking-tight',)}>
               Sign in to your account
             </h2>
           </div>
@@ -48,7 +53,7 @@ export default function SignIn() {
               <div>
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className={classNames( isClicked ? 'text-gray-900':'text-white', 'block text-sm font-medium leading-6',)}
                 >
                   Email address
                 </label>
@@ -60,7 +65,7 @@ export default function SignIn() {
                     required
                     onInput={onHandleUserName}
                     autoComplete="email"
-                    className="block p-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6"
+                    className={classNames( isClicked ? 'text-gray-900':'text-white bg-gray-900','block p-3 w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6',)}
                   />
                 </div>
               </div>
@@ -69,7 +74,7 @@ export default function SignIn() {
                 <div className="flex items-center justify-between">
                   <label
                     htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
+                    className={classNames( isClicked ? 'text-gray-900':'text-white', 'block text-sm font-medium leading-6',)}
                   >
                     Password
                   </label>
@@ -90,7 +95,7 @@ export default function SignIn() {
                     required
                     onInput={onHandleUserPassword}
                     autoComplete="current-password"
-                    className="block p-3 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6"
+                    className={classNames( isClicked ? 'text-gray-900':'text-white bg-gray-900','block p-3 w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 focus:outline-none sm:text-sm sm:leading-6',)}
                   />
                 </div>
               </div>
