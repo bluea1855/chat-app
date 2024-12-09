9e96c8eb62e61cbced604969045f9b0572e50d24
bluea1855 <fakemail1855@gmail.com>
2024-12-09 10:35:33 +0530

    Update BrandsIncluded.jsx

--- src/components/BrandsIncluded.jsx
+++ src/components/BrandsIncluded.jsx
@@ -1,8 +1,11 @@
+import { useSelector } from 'react-redux';
+
 export default function BrandsIncluded() {
+    const isClicked = useSelector((state) => state.click_redux_slice.isClicked);
     return (
-      <div className="bg-white py-24 sm:py-32">
+      <div className={isClicked ? 'bg-white py-24 sm:py-32':'bg-gray-900 py-24 sm:py-32'}>
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+          <h2 className={isClicked ? 'text-gray-900':'text-white','text-center text-lg font-semibold leading-8',}>
             Trusted by the world’s most innovative teams
           </h2>
           <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
@@ -46,4 +49,3 @@ export default function BrandsIncluded() {
       </div>
     )
   }
-  
